91a3c8bf98d807739da6aacfb4aade5d3e852876
jungtaecheon <jongbaseball@gmail.com>
2023-10-02 12:28:56 +0900

    fix

--- treasure_hunt_Mac/reset.sh
+++ treasure_hunt_Mac/reset.sh
@@ -7,4 +7,24 @@ rm user_time_tmp 2>/dev/null
 
 rm hiraite.txt 2>/dev/null
 
+while read -p "> 記録もすべて削除しますか? [y/n] " yes_or_no ; do
+    case ${yes_or_no} in
+        [Yy]|[Yy][Ee][Ss] )
+            rm record.txt 2>/dev/null
+            echo '記録をすべて削除しました。'
+            break
+        ;;
+
+        [Nn]|[Nn][Oo] )
+            echo
+            break
+        ;;
+
+        * )
+            echo
+            echo "> yes か no かでこたえてほしいのです!"
+        ;;
+    esac
+done
+
 echo 'データを初期化しました。'
